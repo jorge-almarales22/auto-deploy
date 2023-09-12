333ca10fd44730d43104f527aac6a31960128682
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-12 18:39:04 -0500

    nueva rama de desarrollo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json({"message": "HOLA MUNDOOOOO!!!!!"})
+  res.json({"message": `Branch ${process.env.BRANCH_NAME}`})
 })
 
 app.listen(3000)
