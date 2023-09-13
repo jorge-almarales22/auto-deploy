75cfdc4f67a7fda85c8ec2e11791f3caf1755364
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-12 19:14:02 -0500

    subiendo nodemon para entorno de desarrollo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express"
+import 'dotenv/config'
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json({"message": `Branch ${process.env.BRANCH_NAME}`})
+  res.json({"message": `ahora estamos subiendo todooos los cambios`})
 })
 
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
+})
